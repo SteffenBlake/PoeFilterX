1fb09c87660627009d469bb45bf591d846fee897
Steffen Cole Blake <steffen.cole.blake@gmail.com>
2022-06-09 18:54:09 -0600

    3.13.4: Code Comment bugfix

--- src/PoeFilterX.Business/Services/FilterBlockParser.cs
+++ src/PoeFilterX.Business/Services/FilterBlockParser.cs
@@ -90,7 +90,7 @@ namespace PoeFilterX.Business.Services
 
                     runningString = "";
                 }
-                else
+                else if (!isComment)
                 {
                     runningString += next;
                 }
